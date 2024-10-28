95bebb8e78d742b644a6e59a6e520f56a53e8ca7
shizhi <lixiangqian.lxq@alibaba-inc.com>
2024-10-28 14:10:15 +0800

    feat: update to 0.76.0, but only iOS

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,18 +1,21 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+// import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '@/redux/reducer';
 import rootSaga from '@/redux/saga';
-// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 const logger = createLogger({});
 const sagaMiddleware = createSagaMiddleware();
 const loggerMiddleware = __DEV__ ? [logger] : [];
 
-const middleWares = applyMiddleware(sagaMiddleware, ...loggerMiddleware);
+// const middleWares = applyMiddleware(sagaMiddleware, ...loggerMiddleware);
 
 const generateStore = () => {
-  const store = createStore(rootReducer, middleWares);
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }).concat(sagaMiddleware).concat([...loggerMiddleware]),
+  });
   sagaMiddleware.run(rootSaga);
   return { store };
 };
